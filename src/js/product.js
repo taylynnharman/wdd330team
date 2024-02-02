b428a420bfcdc3ec9cc8b69c58f230cff71eca91
taylynnharman <dyer.taylynn@gmail.com>
2024-02-02 13:47:09 -0700

    Fix bubble with added changes

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,6 @@
 import { getParam } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
 
 import productDetails from "./productDetails.mjs";
 
 const productId = getParam("product");
 productDetails(productId);
-//console.log("productId", findProductById(productId));
