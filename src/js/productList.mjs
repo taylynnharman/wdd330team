3f8f7de90a40b2de5d6b623b25cd0c06688ddc3c
swalloich <nels.jac42@gmail.com>
2024-02-02 13:15:33 -0700

    added functions required in activity 3 page

---
+++ src/js/productList.mjs
@@ -0,0 +1,22 @@
+function productCardTemplate(product) {
+    return `<li class="product-card">
+    <a href="product_pages/index.html?product=">
+    <img
+      src=""
+      alt="Image of "
+    />
+    <h3 class="card__brand"></h3>
+    <h2 class="card__name"></h2>
+    <p class="product-card__price">$</p></a>
+    </li>`;
+}
+
+function renderList() {
+    
+}
+
+export default function productList(selector, category) {
+    // get the element we will insert the list into from the selector.
+    // get the list of products.
+    // render out the product list to the element.
+}
