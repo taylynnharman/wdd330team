ef664ff20fe69511bda04c3e0b7e7469f7dd3323
swalloich <nels.jac42@gmail.com>
2024-02-09 13:22:51 -0700

    added filtering function to renderListWithTemplate

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -29,10 +29,29 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
+/**
+ * 
+ * @param {CallableFunction} templateFn function to be run on the list items
+ * @param {Element} parentElement the element the list items will be added to.
+ * @param {Array} list An array of objects
+ * @param {String} position 
+ * @param {Boolean} clear a boolean indicating whether or not the element provided
+ *                        should be cleared. True by default.
+ */
 export function renderListWithTemplate(templateFn, parentElement, list, position="afterbegin", clear=true) {
+  // don't know if this was the intended solution, but here you go
+  function filterItems(items) {
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].includes("880RT") || items[i].includes("989CG")) {
+        items.splice(i, 1);
+      }
+    }
+  }
+
   if (clear) {
     parentElement.innerHTML = "";
   }
   const htmlStrings = list.map(templateFn);
+  filterItems(htmlStrings)
   parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
 }
