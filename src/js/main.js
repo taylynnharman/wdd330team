913126f2414544c7febf016ef4cc7b4686e4e74a
taylynnharman <dyer.taylynn@gmail.com>
2024-02-10 13:19:32 -0700

    Fix dynamic header and footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,5 @@
 import productList from "./productList.mjs";
-import {loadTemplate, renderWithTemplate} from "./utils.mjs";
-import { renderWithTemplate } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
+loadHeaderFooter();
 productList(".product-list", "tents");
-
-//render out included header and footer with js
-const header = document.getElementById('header');
-const footer = document.getElementById('footer');
-
-const headerCallback = loadTemplate(path);
-const footerCallback = loadTemplate(path);
-
-renderWithTemplate(headerCallback, header);
-renderWithTemplate(footerCallback, footer);
