f618c2901501836b11f25df5c4fa12511b67b77f
RedMatOnyx <64098861+RedMatOnyx@users.noreply.github.com>
2024-02-14 22:44:38 -0800

    getting started and under construction... deciding where to put the flag

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -12,6 +12,17 @@ function renderProductDetails() {
 
   document.querySelector("#productNameWithoutBrand").innerText =
     product.NameWithoutBrand;
+// discount flag showing dollar amount of discount taken off (ie: $20 off)
+// UNDER CONSTRUCTION, and still deciding where to put it, next to name or over picture, etc.
+let biggerPrice = product.SuggesteRetailPrice;
+let smallerPrice = product.ListPrice;
+let dollarsDiff = biggerPrice-smallerPrice;
+const dollarsLess = document.createElement("p");
+dollarsLess.innerText = `${biggerPrice}`;
+// dollarsLess.innerHTML = "40 dollars off!";
+
+document.querySelector("#productNameWithoutBrand").appendChild(dollarsLess);
+
   document.querySelector("#productImage").src = product.Image;
   document.querySelector("#productImage").alt = product.Name;
   document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
