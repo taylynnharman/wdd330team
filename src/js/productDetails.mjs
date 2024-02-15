b6764b46a37b8896fe6ae0f4096c6e319d3989d7
RedMatOnyx <64098861+RedMatOnyx@users.noreply.github.com>
2024-02-14 21:48:16 -0800

    discount indicator (including percent off and crossed out original price) complete

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -15,14 +15,30 @@ function renderProductDetails() {
   document.querySelector("#productImage").src = product.Image;
   document.querySelector("#productImage").alt = product.Name;
   document.querySelector("#productFinalPrice").innerText = `$${product.FinalPrice}`;
+
+// [][][][][] start of visual discount indicator block [][][][][][]
+  // calculates and displays percent discount in contrasting fontstyle
+  let srPrice = product.SuggestedRetailPrice;
+  let fPrice = product.FinalPrice;
+  let discount = ((srPrice-fPrice)/srPrice).toFixed(2);
+
+  const discountPercent = document.createElement("span");
+  discountPercent.id = "discountSpan";
+  discountPercent.style.color = "orange";
+  discountPercent.style.fontSize = "80%";
+  discountPercent.innerHTML = ` -${discount}% `;
+
+  // displays suggested retail price in in contrasting fontstyle
+  const priceWas = document.createElement("del");
+  priceWas.innerHTML = ` $${srPrice.toFixed(2)}`;
+  priceWas.style.fontSize = "80%";
+  priceWas.style.color = "gray";
   
-  // discount visual indicator
-  let sp = product.SuggestedRetailPrice;
-  let fp = product.FinalPrice;
-  let discount = ((sp-fp)/sp).toFixed(2);
-  const indicator = document.createElement("span");
-  indicator.innerHTML = ` -${discount}% was $${sp.toFixed(2)}`;
-  document.querySelector("#productFinalPrice").appendChild(indicator);
+  document.querySelector("#productFinalPrice").appendChild(discountPercent);
+  document.querySelector("#productFinalPrice").appendChild(priceWas);
+
+// [][][][][][] end of discount indicator block [][][][][][]
+
 
   document.querySelector("#productColorName").innerText =
     product.Colors[0].ColorName;
