e2c71a98c8bfef06bce049bc0df8f57dd73c7ad0
RedMatOnyx <64098861+RedMatOnyx@users.noreply.github.com>
2024-02-24 21:35:23 -0800

    cart total and checkout link, form in checkout page, rough, needs css,etc

--- src/js/cart.js
+++ src/js/cart.js
@@ -34,5 +34,29 @@ function cartItemTemplate(item) {
 
   return newItem;
 }
+//from instructor solution -ma
+export default function shoppingCart() {
+  const cartItems = getLocalStorage("so-cart");
+  const outputEl = document.querySelector(".product-list");
+  renderListWithTemplate(cartItemTemplate, outputEl, cartItems);
+  const total = calculateListTotal(cartItems);
+  displayCartTotal(total);
+}
+// instructor solution -ma
+function displayCartTotal(total) {
+  if (total > 0) {
+    // show our checkout button and total if there are items in the cart.
+    document.querySelector(".list-footer").classList.remove("hide");
+    document.querySelector(".list-total").innerText += ` $${total}`;
+  } else {
+    document.querySelector(".list-footer").classList.add("hide");
+  }
+}
+//from instructor solution -ma
+function calculateListTotal(list) {
+  const amounts = list.map((item) => item.FinalPrice);
+  const total = amounts.reduce((sum, item) => sum + item, 0);
+  return total;
+}
 
 renderCartContents();
