b7b3a50a9cc3fcc2965290d4ec2263a23736d335
taylynnharman <dyer.taylynn@gmail.com>
2024-02-28 12:05:21 -0700

    add footer to config

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         product_pages: resolve(__dirname, "src/product_pages/index.html"),
         product_list: resolve(__dirname, "src/product-list/index.html"),
         header: resolve(__dirname, "src/partials/header.html"),
+        footer: resolve(__dirname, "src/partials/footer.html"),
       },
     },
   },
