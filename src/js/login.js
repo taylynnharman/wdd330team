452596c937687577af49e068446cb9e5a1eeb22b
swalloich <nels.jac42@gmail.com>
2024-03-27 15:14:40 -0600

    added login/index.html styles & started work on js

--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,22 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
+import { login, checkLogin, isTokenValid } from "./auth.mjs";
 
-loadHeaderFooter();
+const redirect = getParam("redirect");
+loadPage();
+
+function loadPage() {
+    loadHeaderFooter();
+    addListeners();
+
+    function getLoginInfo(event) {
+        const email = document.getElementById("email").value;
+        const pass = document.getElementById("password").value;
+
+        login( {email: email, pass: pass}, redirect);
+    }
+
+    function addListeners() {
+        const loginButton = document.getElementById("login-button");
+        loginButton.addEventListener('click', getLoginInfo);
+    }
+}
